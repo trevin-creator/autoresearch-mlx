db6b61b42f86588354da039ba0cf9d68abd11cf0
pascal-maker <pascal-musa@hotmail.com>
2026-07-01 22:23:39 +0200

    experiment: remove logit cap

--- train.py
+++ train.py
@@ -206,7 +206,6 @@ class GPT(nn.Module):
         x = norm(x)
 
         logits = self.lm_head(x).astype(mx.float32)
-        logits = 15.0 * mx.tanh(logits / 15.0)
 
         if targets is None:
             return logits
